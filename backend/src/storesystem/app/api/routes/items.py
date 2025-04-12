4221f48e015c6c5ee6dc00b4a285d6e4aff87fb2
n4okins <63445867+n4okins@users.noreply.github.com>
2025-04-13 05:37:41 +0900

    get_current_itemを追加

--- backend/src/storesystem/app/api/routes/items.py
+++ backend/src/storesystem/app/api/routes/items.py
@@ -1,12 +1,25 @@
-import datetime
+from typing import Optional
 
 import postgrest
 from fastapi import APIRouter, HTTPException
-from pydantic import ValidationError
 from storesystem.app.api.supabase_client import fetch_data, get_table
 from storesystem.models import Item, ParchaseLog, RestockLog
 
 items_router = APIRouter(prefix="/items", tags=["items"])
+
+
+@items_router.get("/")
+def get_current_item(item_id: str) -> Optional[Item]:
+    items = [
+        Item(**item)
+        for item in (
+            get_table("item_stocks").select("*").eq("item_id", item_id).execute()
+        ).data
+    ]
+    if items:
+        return items.pop(0)
+    else:
+        return None
 
 
 @items_router.get("/list")
@@ -30,19 +43,9 @@ def restock_item(restock_log: RestockLog):
         item_quantity (int): 商品の個数
         item_name (Optiona[str]): 追加した商品の名前 (新規追加のときに使用)
     """
-    current_items = [
-        Item(**item)
-        for item in (
-            get_table("item_stocks")
-            .select("*")
-            .eq("item_id", restock_log.item_id)
-            .execute()
-        ).data
-    ]
+    current_item = get_current_item(item_id=restock_log.item_id)
 
-    if current_items:  # 既に商品データがある場合は現在の個数取得 + Update
-        # item_idで取得したため要素が1つであることは保証されている
-        current_item = current_items.pop()
+    if current_item:  # 既に商品データがある場合は現在の個数取得 + Update
         get_table("item_stocks").update(
             dict(item_quantity=current_item.item_quantity + restock_log.item_quantity)
         ).eq("item_id", restock_log.item_id).execute()
@@ -53,48 +56,39 @@ def restock_item(restock_log: RestockLog):
             item_quantity=restock_log.item_quantity,
         )
         get_table("item_stocks").insert(item.model_dump(mode="json")).execute()
+
     try:
         get_table("restock_log").insert(restock_log.model_dump_for_log()).execute()
         return {"message": "Success!"}
     except postgrest.exceptions.APIError as e:
+        # User IDが間違っている場合など？
         raise HTTPException(status_code=404, detail=e.details)
 
 
 @items_router.post("/parchase")
 def parchase_item(parchase_log: ParchaseLog):
-    try:
-        current_items = [
-            Item(**item)
-            for item in (
-                get_table("item_stocks")
-                .select("*")
-                .eq("item_id", parchase_log.item_id)
-                .execute()
-            ).data
-        ]
-    except ValidationError:
-        raise HTTPException(
-            status_code=404, detail=f"ItemID={parchase_log.item_id}の在庫は0です。"
-        )
+    current_item = get_current_item(item_id=parchase_log.item_id)
 
-    if len(current_items) == 0:
+    if current_item is None:
         raise HTTPException(
             status_code=404,
             detail=f"ItemID={parchase_log.item_id}が存在しません。",
         )
-    # item_idで取得したため要素が1つであることは保証されている
-    current_item = current_items.pop()
+
     if current_item.item_quantity - parchase_log.item_quantity < 0:
         raise HTTPException(
             status_code=404, detail="購入する商品の在庫がマイナスになってしまいます！"
         )
+
     get_table("item_stocks").update(
         dict(item_quantity=current_item.item_quantity - parchase_log.item_quantity)
     ).eq("item_id", parchase_log.item_id).execute()
+
     try:
         get_table("parchase_log").insert(parchase_log.model_dump_for_log()).execute()
         return {"message": "Success!"}
     except postgrest.exceptions.APIError as e:
+        # User IDが間違っている場合など？
         raise HTTPException(status_code=404, detail=e.details)
 
 
